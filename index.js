2f31a60d85ab44d110a3ef7a17dbd3e88b1a98da
Winston <winstonnau007@gmail.com>
2021-04-10 12:24:37 +0200

    :sparkles: added clear and undo requests

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
-const io = require('socket.io')(server);
+const io = require("socket.io")(server);
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('newPathDrawn', (path) => {
-        socket.broadcast.emit('newPath', path);
-    })
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("newPathDrawn", (path) => {
+    socket.broadcast.emit("newPath", path);
+  });
+  socket.on("clearReq", () => {
+    socket.broadcast.emit("clear");
+  });
+  socket.on("undoReq", () => {
+    socket.broadcast.emit("undo");
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 server.listen(process.env.PORT, () => {
-    console.log('listening on', process.env.PORT);
+  console.log("listening on", process.env.PORT);
 });
