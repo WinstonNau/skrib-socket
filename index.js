68c49a158b6a8d1ccc2d039d883d1e5f6255e29c
Winston <winstonnau007@gmail.com>
2021-04-10 11:15:24 +0200

    :tada: initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('newPathDrawn', (path) => {
+        socket.broadcast.emit('newPath', path);
+    })
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
