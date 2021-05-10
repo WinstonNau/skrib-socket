596330926d22580dcd1e2eb909a7cac7096cb775
Winston <winstonnau007@gmail.com>
2021-05-10 23:33:28 +0200

    :construction:

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server);
 
+let userMap = Map();
+let gamePlayersMap = Map();
+
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
   socket.on("playerJoined", (gameId, playerId, playerUsername) => {
+    userMap.set(socket.id, playerId);
     socket.broadcast.emit("newPlayer", gameId, playerId, playerUsername);
   });
   socket.on("playerLeave", (gameId, playerId) => {
@@ -21,7 +25,9 @@ io.on("connection", (socket) => {
   socket.on("undoReq", (gameId) => {
     socket.broadcast.emit("undo", gameId);
   });
-  socket.on("startGame", (gameId) => {
+  socket.on("startGame", (gameId, users) => {
+    gamePlayersMap.set(gameId, users);
+    console.log(gamePlayersMap.get(gameId));
     socket.broadcast.emit("gameStarted", gameId);
   });
   socket.on("selectWord", (gameId, word) => {
@@ -34,7 +40,18 @@ io.on("connection", (socket) => {
     //TODO: Maybe define the next player? Or maybe define an order of players when the round starts?
     socket.broadcast.emit("playerGuessedCorrect", gameId, playerUsername);
   });
+  //Test this:
+  socket.on("gameIsOver", (gameId) => {
+    userMap.delete(socket.id);
+    //TODO: Add other game over stuff
+  });
+  //Test this:
   socket.on("disconnect", () => {
+    if (userMap.has(socket.id)) {
+      //TODO: Remove the player from the game
+      //TODO: Send an event to the other players to notify them, that a player left the game and choose the next player or something like that
+      userMap.delete(socket.id);
+    }
     console.log("user disconnected", socket.id);
   });
 });
